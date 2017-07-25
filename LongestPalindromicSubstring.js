ffdea283b06425becb8e1358fb03964685cb0c58
Luis J. Romero <romerolanguages@gmail.com>
2017-07-25 18:49:27 -0400

    Update

--- LongestPalindromicSubstring.js
+++ LongestPalindromicSubstring.js
@@ -12,16 +12,17 @@ function LongestPalindromicSubstring() {
 		  var max_len = 1;//length of the longest palindrome
 		  
 		  // how to fix/change boolean from Java to JavaScript?
+		  // search how to create two-dimensional arrays in javascript
 		  boolean palindrome[][] = new boolean[n][n]; //boolean table to store palindrome truth
 		  
 		  //Trivial case: single letter palindromes
-		  for (int i = 0; i < n; i++) {
+		  for (let i = 0; i < n; i++) {
 			  palindrome[i][i] = true;
 		  }//end of for
 		  
 		  //Finding palindromes of two characters.
-		  for (int i = 0; i < n-1; i++) {
-		    if (s.charAt(i) == s.charAt(i+1)) {
+		  for (let i = 0; i < n-1; i++) {
+		    if (str.charAt(i) == str.charAt(i+1)) {
 		      palindrome[i][i+1] = true;
 		      palindromeBeginsAt = i;
 		      max_len = 2;
@@ -29,10 +30,10 @@ function LongestPalindromicSubstring() {
 		  } //end of for
 		  
 		  //Finding palindromes of length 3 to n and saving the longest
-		  for (int curr_len = 3; curr_len <= n; curr_len++) {
-		    for (int i = 0; i < n-curr_len+1; i++) {
-		      int j = i+curr_len-1;
-		      if (s.charAt(i) == s.charAt(j) //1. The first and last characters should match 
+		  for (let curr_len = 3; curr_len <= n; curr_len++) {
+		    for (let i = 0; i < n-curr_len+1; i++) {
+		      let j = i+curr_len-1;
+		      if (str.charAt(i) == str.charAt(j) //1. The first and last characters should match 
 		    	  && palindrome[i+1][j-1]) //2. Rest of the substring should be a palindrome
 		      {
 		    	palindrome[i][j] = true; 
@@ -41,7 +42,7 @@ function LongestPalindromicSubstring() {
 		      }//end of if
 		    }//end of for
 		  }//end of for
-		  return s.substring(palindromeBeginsAt, max_len + palindromeBeginsAt);
+		  return str.substring(palindromeBeginsAt, max_len + palindromeBeginsAt);
 		}//end of public
 
 
